6c2dce15338720cab9078023d00276c40c03eeab
fortydeux <ogtpowell@gmail.com>
2025-09-13 08:04:30 -0400

    Updates flake inputs: determinate, home-manager, hyprland, hyprland-plugins, niri; removes deprecated pcloud nix files; toggles off several modules for killifish host for 'slim build' due to space constrains.

--- nixos-config/hosts/killifish/configuration.nix
+++ nixos-config/hosts/killifish/configuration.nix
@@ -13,13 +13,13 @@
     ../../system-modules/extraPackages.nix
     ../../system-modules/display-manager.nix
     ../../system-modules/plasma.nix
-    ../../system-modules/window-managers.nix
-    ../../system-modules/cosmic-desktop.nix
+    # ../../system-modules/window-managers.nix
+    # ../../system-modules/cosmic-desktop.nix
     # ../../system-modules/pcloud.nix
-    ../../system-modules/extraFonts.nix
+    # ../../system-modules/extraFonts.nix
     # ../../system-modules/virtualisation.nix
     # ../../system-modules/audio-prod.nix
-    ../../system-modules/fun-and-games.nix
+    # ../../system-modules/fun-and-games.nix
     # Home-manager
     inputs.home-manager.nixosModules.home-manager
     # Device-specific
